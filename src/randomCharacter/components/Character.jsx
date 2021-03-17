bd9736b5972bd1923ee6c7e901f846e5941ae1c5
bzdv21 <boaz.dev.21+github@gmail.com>
2021-03-18 01:02:37 +0200

    Small refactor - using react-konnekt

--- src/randomCharacter/components/Character.jsx
+++ src/randomCharacter/components/Character.jsx
@@ -2,8 +2,18 @@ import { useEffect } from "react";
 import { useRandomCharacterContext } from "../context";
 import "./style.css";
 import spinner from "./three-dots.svg";
+import Konnekt from "react-konnekt";
 
-export function Character() {
+function CharacterCard({ name, ImageComponent }) {
+  return (
+    <section className="character">
+      <ImageComponent />
+      <h2>{name}</h2>
+    </section>
+  );
+}
+
+export const Character = Konnekt(() => {
   const { state, dispatch } = useRandomCharacterContext();
 
   useEffect(() => {
@@ -12,45 +22,46 @@ export function Character() {
     }, 2000);
   }, [dispatch]);
 
-  return (
-    <section className="character">
-      {["init", "loading"].includes(state.status) ? (
-        <>
-          <div
-            role="img"
-            aria-label="breaking bad logo"
-            style={{
-              backgroundColor: "#fff",
-              width: "200px",
-              height: "300px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img src={spinner} alt="spinner" />
-          </div>
-          {state.status === "loading" ? <h3>Loading data...</h3> : null}
-          {state.status === "init" ? <h3>Initializing application</h3> : null}
-        </>
-      ) : null}
-      {["idle", "error"].includes(state.status) ? (
-        <>
-          <div
-            role="img"
-            aria-label={`image of ${state.character.name}`}
-            style={{
-              backgroundImage: `url("${state.character.img}")`,
-              backgroundColor: "#369457 ",
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              width: "200px",
-              height: "300px",
-            }}
-          />
-          <h3>{state.character.name}</h3>
-        </>
-      ) : null}
-    </section>
-  );
-}
+  if (["idle", "error"].includes(state.status)) {
+    return {
+      name: state.character.name,
+      ImageComponent: () => (
+        <div
+          role="img"
+          aria-label={`image of ${state.character.name}`}
+          style={{
+            backgroundImage: `url("${state.character.img}")`,
+            backgroundColor: "#369457 ",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            width: "200px",
+            height: "300px",
+          }}
+        />
+      ),
+    };
+  }
+
+  return {
+    name:
+      state.status === "loading"
+        ? "Loading data..."
+        : "Initializing application",
+    ImageComponent: () => (
+      <div
+        role="img"
+        aria-label="breaking bad logo"
+        style={{
+          backgroundColor: "#fff",
+          width: "200px",
+          height: "300px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src={spinner} alt="spinner" />
+      </div>
+    ),
+  };
+})(CharacterCard);
